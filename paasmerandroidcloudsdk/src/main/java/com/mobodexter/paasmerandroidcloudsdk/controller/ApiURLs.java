0a6b230ba0c1be0340cee211538c6d8ad4acf46d
Arun <arunkumar.v@mobodexter.com>
2018-01-03 14:59:40 +0530

    changes in url

--- paasmerandroidcloudsdk/src/main/java/com/mobodexter/paasmerandroidcloudsdk/controller/ApiURLs.java
+++ paasmerandroidcloudsdk/src/main/java/com/mobodexter/paasmerandroidcloudsdk/controller/ApiURLs.java
@@ -5,13 +5,13 @@ package com.mobodexter.paasmerandroidcloudsdk.controller;
  */
 
 public class ApiURLs {
-    public static final String SING_UP="http://54.218.77.1:3000/api/auth/signup";
-    public static final String SING_IN="http://54.218.77.1:3000/api/auth/login";
-    public static final String EMAIL_VERIFY="http://192.168.100.130:3000/api/auth/validate";
-    public static final String EMAIL_VERIFY_RESEND="http://192.168.100.130:3000/api/auth/resend";
-    public static final String FORGOT_PASSWORD="http://54.218.77.1:3000/api/auth/forgot";
-    public static final String PASSWORD_RESET="http://54.218.77.1:3000/api/auth/changepassword";
-    public static final String MANAGE_DEVICES="http://54.218.77.1:3000/api/device";
-    public static final String GET_DEVICES="http://192.168.100.130:3000/api/auth/validate";
+    public static final String SING_UP="https://developers.paasmer.co/api/auth/signup";
+    public static final String SING_IN="https://developers.paasmer.co/api/auth/login";
+    public static final String EMAIL_VERIFY="https://developers.paasmer.co/api/auth/validate";
+    public static final String EMAIL_VERIFY_RESEND="https://developers.paasmer.co/api/auth/resend";
+    public static final String FORGOT_PASSWORD="https://developers.paasmer.co/api/auth/forgot";
+    public static final String PASSWORD_RESET="https://developers.paasmer.co/api/auth/changepassword";
+    public static final String MANAGE_DEVICES="https://developers.paasmer.co/api/device";
+    public static final String GET_DEVICES="https://developers.paasmer.co/api/auth/validate";
 
 }
